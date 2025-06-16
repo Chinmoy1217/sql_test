ce105f9453b4065f52b3e1e03aa994eab1c4905a
Chinmoy Jagjit <manoj1234567890zdrwa@gmail.com>
2025-06-16 13:04:02 +0530

    added new Tables

---
+++ dbo/Tables/Customs.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Customs] (
+    [CustomID]   INT      NOT NULL,
+    [CustomDate] DATETIME NOT NULL,
+    PRIMARY KEY CLUSTERED ([CustomID] ASC)
+);
+GO
+
---
+++ dbo/Tables/Passenger.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Passenger] (
+    [PassID]     INT      NOT NULL,
+    [TravelDate] DATETIME NOT NULL,
+    PRIMARY KEY CLUSTERED ([PassID] ASC)
+);
+GO
+
