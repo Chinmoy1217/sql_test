9a06e9f012526eccf841bec93f60c3fed0812572
Chinmoy Jagjit <manoj1234567890zdrwa@gmail.com>
2025-06-16 08:10:20 +0530

    changed

--- dbo/Tables/Users.sql
+++ dbo/Tables/Users.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[Users] (
-    [UserID]   INT           NOT NULL,
-    [UserName] NVARCHAR (50) NOT NULL,
+    [UserID]     INT           NOT NULL,
+    [UserName]   NVARCHAR (50) NOT NULL,
+    [TestColumn] INT           NULL,
     PRIMARY KEY CLUSTERED ([UserID] ASC)
 );
 
